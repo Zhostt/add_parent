2a0228a82b0c86a4ad53b5a68b384e2ef821cb37
Zhostt <crazyplumber@yandex.ru>
2023-10-05 15:18:07 +0300

    finished linking inputs to form screen, issue: resetting the forms dont work properly, should fix

--- src/components/InputChildren.vue
+++ src/components/InputChildren.vue
@@ -7,14 +7,14 @@
       >
         +
       </button>
-      <div class="child-form-block" v-for="input in formInputs" :key="input.id">
+      <div class="child-form-block" v-for="input in childrenInputs" :key="input.id">
         <label for="name">
           Имя
-          <input type="text" name="name" required v-model="input.name">
+          <input type="text" name="name" required v-model="input.name" autocomplete="off">
         </label>
         <label for="children">
           Возраст
-          <input type="text" name="children" required v-model="input.age">
+          <input type="text" name="children" required v-model="input.age" autocomplete="off">
         </label>
         <button class="btn delete-form-btn" @click="handleDelete(input.id)">Удалить</button>
       </div>
@@ -31,18 +31,18 @@ interface Input {
   parentId: number | undefined;
 }
 // Состояние - массив данных с форм + статус кнопки добавления формы
-const formInputs = ref<Input[]>([]);
+const childrenInputs = ref<Input[]>([]);
 const addFormInactive = ref<boolean>(false);
 
 // Обработка добавления формы. Пушим в массив объект формы, увязанной с темплейтом
 // Останавливаем добавление новых форм на 5
 const addForm = () => {
-  if (formInputs.value.length < 5) {
+  if (childrenInputs.value.length < 5) {
     const id = Date.now();
     const newInput: Input = {
       name: '', age: null, id, parentId: undefined,
     };
-    formInputs.value.push(newInput);
+    childrenInputs.value.push(newInput);
   } else {
     addFormInactive.value = true;
   }
@@ -50,12 +50,12 @@ const addForm = () => {
 
 // Обработка удаления формы через id
 const handleDelete = (id: number) => {
-  formInputs.value = formInputs.value.filter((input) => input.id !== id);
+  childrenInputs.value = childrenInputs.value.filter((input) => input.id !== id);
 };
 // Эмит на вочере за состоянием форм
 // для отправки данных в родительский компонент при любом изменении
 const emits = defineEmits(['children-change']);
-watch(formInputs.value, (newVal) => {
+watch(childrenInputs.value, (newVal) => {
   emits('children-change', newVal);
 });
 
--- src/components/InputParent.vue
+++ src/components/InputParent.vue
@@ -8,6 +8,8 @@
           placeholder="Имя"
           name="name"
           required
+          v-model = "parentInputs.name"
+          autocomplete="off"
         />
       </label>
     </div>
@@ -19,14 +21,28 @@
           placeholder="Возраст"
           name="age"
           required
+          v-model = "parentInputs.age"
+          autocomplete="off"
         />
       </label>
     </div>
   </div>
 </template>
 
-<script setup>
+<script lang='ts' setup>
+import { ref, watch, defineEmits } from 'vue';
 
+const parentInputs = ref({
+  name: '' as string,
+  age: null as number | null,
+});
+// Эмит на вочере за состоянием форм
+// для отправки данных в родительский компонент при любом изменении
+const emits = defineEmits(['parent-change']);
+watch(parentInputs.value, (newVal) => {
+  emits('parent-change', newVal);
+  parentInputs.value = newVal;
+});
 </script>
 
 <style lang="scss" scoped></style>
